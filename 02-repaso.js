f1f9296d7fb050a06e54d5db53419b55895b457e
Angelhdez15 <luisangelhernandezflores604@gmail.com>
2024-08-25 14:35:29 -1200

    tarea

--- 02-repaso.js
+++ 02-repaso.js
@@ -5,15 +5,49 @@ A.map((datos,index)=>{
     datos.map((i,valores)=>{
         console.log(`${i}=${valores}`);
     })
-})
+});
 }
 datosArreglos()
 // Tarea hacer lo mismo de lo anterior usando los siguientes
 //for of
+let B=[[7,8,9],[1,2,3]]
+for (let arreglo of B){
+    for(let nuemros of arreglo){ 
+        console.log(nuemros);
+    }
+}
 //for in
+let C=[[9,8,7],[6,5,4]]
+for(let i in C){
+    for (let elementos in C[1]){
+        console.log(C[i][elementos]);
+        
+    }
+}
 //for each
+let D=[[4,3,2],[1,5,6]]
+D.forEach(Areglo=>{
+    Areglo.forEach(elemto=>{
+        console.log(elemto);
+        
+    })
+})
 //funcion flecha
+let E=[[5,4,6],[8,7,9]]
+let array=(wr)=>wr.flat();
+console.log(array(E));
+
 //while
+let F=[[9,5,4],[6,2,1]]
+let i=0
+while(i<F.length){
+    let segn=0
+    while(segn<F[i].length){
+        console.log(F[i][segn]);
+        segn++
+    }
+    i++
+}
 //Estructura de control parser
 let a=4;
 let b=5;
@@ -25,3 +59,4 @@ if (A>b){
 }}
 const comparar=a>b?"A es mayor":"B es mayor";
 console.log(comparar);
+datos();
