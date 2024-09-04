0782d5948b7d59e3450616125039621b9a08a634
Angelhdez15 <luisangelhernandezflores604@gmail.com>
2024-09-03 16:13:39 -1200

    modificacion a los ejercicios

--- 04-instrucciones.js
+++ 04-instrucciones.js
@@ -1,47 +1,60 @@
-const prompt=require(`prompt-sync`)();
-/*let numero1;
-let numero2;
-numero1=parseInt(prompt("ingrese el numero 1"))
-numero2=parseInt(prompt("ingrese el numero 2"))
-console.log(numero1*numero2);
+const prompt = require("prompt-sync")();
 
-Realizar un programa que contenga un arreglo indefinido en que solicite a travez del teclado el tamaño 
-y se ingrese por lo menos 4 valores de cualquier tipo, utilizando un ciclo para ingresar datos, imprimir los valores en pantalla
-let arreglo=[]
-function info(){
-    const dimension=parseInt(prompt("ingresa el tamaño del arreglo:"))
-    for (let i = 0; i < dimension; i++) {
-        let datos =prompt("ingresa los datos:");
-        //arreglo[i]=datos
-        arreglo.push(datos)
+/* let numero1;
+let numero2;
+
+numero1=parseInt(prompt("Ingrese el nuumero 1: "));
+numero2=parseInt(prompt("Ingresa el valor 2: "));
+
+
+console.log(numero1*numero2);
+ */
+
+/* Realizar un programa que contenga un arreglo indefinido,
+en el que solicite a través de teclado el tamaño y 
+se ingrese por lo menos 4 valores de cualquier tipo,
+urtilizando un ciclo para ingresar los datos
+imprimir los valores en pantalla. */
+
+let arreglo = [];
+/* 
+function infoArreglo(){
+    const dimension=parseInt(prompt('Ingresa el tamanio del arreglo: '));
+    for(let i=0; i<dimension;i++){
+        let datos=prompt("Ingresa los datos: ")
+        //arreglo[i]=datos;
+        arreglo.push(datos);
     }
-    arreglo.map((Data)=>{
-        console.log(Data);
+    //console.log(arreglo);
+    
+    arreglo.map((data)=>{
+        console.log(data);
         
     })
- }
- info()*/
-let arreglo=[]
-let informacion;
-const datos=()=>{
-    const dimension=parseInt(prompt("ingresa el tamaño del arreglo:"))
-    for (let i = 0; i < dimension; i++) {
-        let nombre=prompt("ingresa tu nombre:");
-        let edad=perseint(prompt("ingresa tu nombre:"));
-        informacion={
-            nombre,
-            edad,
-        };
-        areglo.push(informacion)
-}
-arreglo.map((precioso)=>(
-    console.log(`El nombre es ${precioso.nombre} y tiene ${precioso.edad}`)
     
-));
-console.log(arreglo);
+} */
 
+//infoArreglo();
+
+let informacion;
+const datos = () => {
+  const dimension = parseInt(prompt("Ingresa el tamanio del arreglo: "));
+  for (let i = 0; i < dimension; i++) {
+    let nombre = prompt("Ingresa el nombre: ");
+    let edad = parseInt(prompt("Ingresa el nombre: "));
+    informacion = {
+      nombre,
+      edad,
+    };
+    arreglo.push(informacion);
+  }
+
+  arreglo.map((precioso) => {
+    console.log(
+      `El nombre es ${precioso.nombre} y tiene ${precioso.edad} años`
+    );
+  });
+
+  console.log(arreglo);
 };
-datos()
-
-
-
+datos();
--- 06-clases.js
+++ 06-clases.js
@@ -1,25 +1,52 @@
-// Definir clases con atributps dentro del constructor
-class persona{
-    contructor(nombre,edad,sexo,dirrecion){
-        this.nombre=nombre;
-        this.edad=edad;
-        this.sexo=sexo;
-        this.dirrecion=dirrecion;
+//Definir clases con atributos dentro del constructor
+
+class Persona{
+    //Variable estatica: Solo le pertenece a la clase
+    #clave;
+    #iniciales
+
+    //Metodo constructor
+    constructor(nombre, edad, sexo, direccion){
+        this._nombre=nombre;
+        this._edad=edad;
+        this._sexo=sexo;
+        this._direccion=direccion;
+        this.#clave=12076;
+        this.#iniciales="ISC-";
     }
-// Metodo saludar
- saludar(){ //se deja de llamar funcion y ahora es metodo
-console.log(`Hola${this.nombre}`);
-    
+//Metodo saludar
+ saludar(){
+console.log(`Hola soy ${this._nombre} y mi carrera es ${this._carrera} mi matricula es ${this.generarClave()}`);
+
+    }
+
+    //Metodo para generar una clave de alumno
+
+    generarClave(){
+        return this.#iniciales+Math.floor(Math.random()*10*this.#clave)
+    }
+
+    //Encapsulamiento setter y getter
+     getNombre(){
+        return this._nombre
+    }
+
+     setNombre(values){
+        this._nombre=values;
+    }
+
+    getCarrera(){
+        return this._carrera;
+    }
+
+    setCarrera(valor){
+        this._carrera=valor;
+    }
+
 }
-//Encapsulamiento setter y getter
-get nombre(){
-    return this.nombre
-}
-set nombre (valores){
-    this.nombre=valores;
-}
-}
-let persona1=new persona("Dany",23,"M","Balancam");
-persona1.nombe="jesus"
+
+let persona1=new Persona("Dany",23,"M","Balancan");
+persona1.setNombre("Jose");
+persona1.setCarrera("Admin")
 console.log(persona1);
-persona1.saludar()
+persona1.saludar();
