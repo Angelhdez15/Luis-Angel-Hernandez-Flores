3c1c0138eb7e298981349bf786bff5cf67b1a5fb
Angelhdez15 <luisangelhernandezflores604@gmail.com>
2024-09-26 13:26:21 -1200

    ejercicio de clase

---
+++ ejercicio.js
@@ -0,0 +1,20 @@
+class persona{
+    constructor(nombre,edad,sexo,direccion,telefono){
+        this._nombre=nombre
+        this._edad=edad
+        this._sexo=sexo
+        this._direccion=direccion
+        this._telefono=telefono
+    }
+    saludar(){
+        console.log(`hola mi nombre es ${this._nombre} tengo ${this._edad} soy ${this._sexo} mi direccion es ${this._direccion} mi telefono es ${this._telefono}`);
+        
+    }
+}
+
+let persona1=new persona("joel",18,"hombre","villa",345345)
+let persona2=new persona("simon",18,"hombre","balancan",43553)
+let persona3=new persona("diana",19,"mujer","zapata",6764734)
+persona1.saludar()
+persona2.saludar()
+persona3.saludar()
